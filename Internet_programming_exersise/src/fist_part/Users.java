7da4d621f5172ed7fdc770d65b59456b3826aa06
Grispos <Grispos@Grispos-PC>
2018-03-14 18:47:18 +0200

    14/3/2018

---
+++ Internet_programming_exersise/src/fist_part/Users.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package fist_part;
+
+/**
+ * @author xristos
+ *
+ */
+public class Users {
+	private String username, name, surname, department;
+	private static int usersCounter;
+	
+	protected String getUsername() {
+		return username;
+	}
+	protected void setUsername(String username) {
+		this.username = username;
+	}
+	protected String getName() {
+		return name;
+	}
+	protected void setName(String name) {
+		this.name = name;
+	}
+	protected String getSurname() {
+		return surname;
+	}
+	protected void setSurname(String surname) {
+		this.surname = surname;
+	}
+	protected String getDepartment() {
+		return department;
+	}
+	protected void setDepartment(String department) {
+		this.department = department;
+	}
+	protected static int getUsersCounter() {
+		return usersCounter;
+	}
+	protected static void setUsersCounter(int usersCounter) {
+		Users.usersCounter = usersCounter;
+	}
+
+}
